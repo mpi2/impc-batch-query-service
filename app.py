605112b87a266017cc6d3803dacd422688b078d2
Francisco Peña <francisco@ebi.ac.uk>
2024-10-08 17:18:13 +0100

    feat: update dataframe nested data columns to string values
    based on this page: https://community.plotly.com/t/polars-df-in-a-dash-datatable/84585

--- app.py
+++ app.py
@@ -7,6 +7,22 @@ app = Flask(__name__)
 # Load the dataset (assuming it's already stored in the same directory)
 dataset = pl.read_parquet('batch_query_service_data_parquet/*.parquet')
 
+def flatten_all_nested_columns(inputDf):
+    resultDf = inputDf
+    for colName in resultDf.columns:
+        if str(resultDf.select(pl.col(colName)).dtypes[0]) not in ["String", "Int64", "Boolean"]:
+            if str(resultDf.select(pl.col(colName)).dtypes[0]).startswith("Struct"):
+                resultDf = resultDf.with_columns(pl.col(colName).struct.json_encode())
+            elif str(resultDf.select(pl.col(colName)).dtypes[0]).startswith("List"):
+                resultDf = resultDf.with_columns(("[" + pl.col(colName).cast(pl.List(pl.Utf8)).list.join(", ")+ "]").alias(f"stringifiedList_{colName}"))
+
+    for colName in resultDf.columns:
+        if colName.startswith("stringifiedList_"):
+            resultDf = resultDf.drop( colName.replace("stringifiedList_", "") )
+            resultDf = resultDf.rename({colName: colName.replace("stringifiedList_", "")})
+    
+    return resultDf
+
 
 @app.route('/query', methods=['POST'])
 def query_data():
@@ -45,7 +61,9 @@ def dataframe_to_xlsx(df):
 
 def dataframe_to_tsv(df):
     output = io.BytesIO()
-    df.write_csv(output, separator='\t')
+    newDf = flatten_all_nested_columns(df)
+    newDf.write_csv(output, separator='\t')
+
     output.seek(0)
     return send_file(output, mimetype='text/tab-separated-values', as_attachment=True,
                      download_name='query_results.tsv')
